38ed689331549b9e485c245a2ed76e9feb9b3bb5
sark <sarkahn@gmail.com>
2020-01-05 17:15:12 -0700

    Console working, example scene in Scenes/ConsoleTest

---
+++ Assets/RLTK/Console/IConsole.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Collections;
+using Unity.Mathematics;
+using UnityEngine;
+
+
+namespace RLTK.Consoles
+{
+    public interface IConsole
+    {
+        int2 Size { get; }
+
+        int CellCount { get; }
+
+        int2 PixelsPerUnit { get; }
+
+        /// <summary>
+        /// Update internal render data if our tile data has changed.
+        /// </summary>
+        void RebuildIfDirty();
+        
+        void ClearScreen();
+
+        void Resize(int w, int h);
+
+        void Draw(Font font, Material mat);
+
+        int At(int x, int y);
+        
+        void Print(int x, int y, string str);
+
+        void PrintColor(int x, int y, Color32 fgColor, Color32 bgColor, string str);
+
+        void Set(int x, int y, Color32 fgColor, Color32 bgColor, byte glyph);
+
+        void DrawBox(int x, int y, int width, int height, Color32 fgColor, Color32 bgColor);
+
+        void DrawHollowBox(int x, int y, int width, int height, Color32 fgColor, Color32 bgColor);
+
+        void DrawBoxDouble(int x, int y, int width, int height, Color32 fgColor, Color32 bgColor);
+
+        void DrawHollowBoxDouble(int x, int y, int width, int height, Color32 fgColor, Color32 bgColor);
+
+        void FillRegion(IntRect r, byte glyph, Color32 fgColor, Color32 bgColor);
+
+        //void CopyTiles(NativeArray<Tile> buffer);
+
+        //void WriteTiles(NativeArray<Tile> tiles);
+
+        byte? Get(int x, int y);
+    }
+}
