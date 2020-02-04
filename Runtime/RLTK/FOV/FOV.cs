25cb873148e4ed95c62accea84503c8df2c6e233
sark <sarkahn@gmail.com>
2020-02-04 07:04:31 -0700

    0.2.5 - 2020-02-04 * Reworked FOV API to be usable inside external jobs

--- Runtime/RLTK/FOV/FOV.cs
+++ Runtime/RLTK/FOV/FOV.cs
@@ -11,63 +11,60 @@ namespace RLTK
 {
     public static class FOV
     {
+        /// <summary>
+        /// This interface can be implemented in a struct which can then be used inside an <see cref="FOVJob{T}"/>
+        /// </summary>
         public interface IVisibilityMap
         {
             bool IsOpaque(int2 p);
             bool IsInBounds(int2 p);
         }
 
-        public static NativeList<int2> GetVisiblePoints<T>(int2 origin, int range, T visibilityMap) where T : IVisibilityMap
-        {
-            NativeList<int2> points = new NativeList<int2>((range * 2) * (range * 2), Allocator.TempJob);
-
-            new FOVJob<T>
-            {
-                origin = origin,
-                range = range,
-                visiblePoints = points,
-                map = visibilityMap
-            }.Run();
-            
-            return points;
-        }
-
-        public static JobHandle ScheduleGetVisiblePoints<T>(int2 origin, int range, T visibilityMap, NativeList<int2> pointsBuffer, JobHandle inputDeps)
-            where T : IVisibilityMap
+        /// <summary>
+        /// <para>Utility function to make the syntax for scheduling/running an FOV job a little nicer. Usage:</para>
+        /// <para>FOV.GetVisiblePointsJob(0,5,map,points).Run()</para>
+        /// <para>var job = FOV.GetVisiblePointsJob(0,5,map,points); job.Schedule();</para>
+        /// </summary>
+        public static FOVJob<T> GetVisiblePointsJob<T>(int2 origin, int range, T visibilityMap, NativeList<int2> buffer) where T : IVisibilityMap
         {
             return new FOVJob<T>
             {
                 origin = origin,
                 range = range,
-                visiblePoints = pointsBuffer,
-                map = visibilityMap
-            }.Schedule(inputDeps);
+                map = visibilityMap,
+                buffer = buffer
+            };
         }
 
-
         [BurstCompile]
-        struct FOVJob<T> : IJob where T : IVisibilityMap
+        public struct FOVJob<T> : IJob where T : IVisibilityMap
         {
             public T map;
 
-            public NativeList<int2> visiblePoints;
+            public NativeList<int2> buffer;
 
             public int2 origin;
             public int range;
 
             public void Execute()
             {
-                BresenhamCircle circle = new BresenhamCircle(origin, range);
-                var points = circle.GetPoints(Allocator.Temp);
-                for( int i = 0; i < points.Length; ++i )
-                {
-                    var p = points[i];
-
-                    ScanFOVLine(origin, p, map, visiblePoints);
-                }
+                GetVisiblePoints(origin, range, map, buffer);
             }
         }
-        
+
+        public static void GetVisiblePoints<T>(int2 origin, int range, T visibilityMap, NativeList<int2> pointsBuffer)
+            where T : IVisibilityMap
+        {
+            BresenhamCircle circle = new BresenhamCircle(origin, range);
+            var points = circle.GetPoints(Allocator.Temp);
+            for (int i = 0; i < points.Length; ++i)
+            {
+                var p = points[i];
+
+                ScanFOVLine(origin, p, visibilityMap, pointsBuffer);
+            }
+        }
+
         static void ScanFOVLine<T>(int2 start, int2 end, T map, NativeList<int2> visiblePoints) where T : IVisibilityMap
         {
             var line = new VectorLine(start, end);
